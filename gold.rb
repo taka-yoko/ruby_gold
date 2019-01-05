2eda8aa1ada52b3f8a8da43cfdf3d1d91b4c30cb
yokoyan <2511649+taka-yoko@users.noreply.github.com>
2019-01-05 22:32:06 +0900

    Update gold.rb

--- gold.rb
+++ gold.rb
@@ -1,7 +1,6 @@
 # Ruby Association Certified Ruby Programmer Gold 学習のための備忘録。学んだことのメモと復習に使用
 
 # REx より
-
 # 定数の参照はレキシカルに行われる(関数、クラスを定義した時点でスコープが決まる)
 # 下記の場合、M::C#awesome_methodのコンテキストにCONSTが見つからないため、例外が発生する。
 # M::CONSTであれば"Hello, world"と表示される。
@@ -18,7 +17,8 @@ end
 
 p M::C.new.awesome_method
 # => `awesome_method': uninitialized constant M::C::CONST (NameError)
-#####
+
+############################################################
 
 # 整数、少数の演算
 p 4/5
@@ -37,6 +37,8 @@ class C
 end
 # => VAR = v としている所で dynamic constant assignment
 # メソッド内の定数の更新は文法エラーになる
+
+############################################################
 
 # 可変長引数
 def hoge *args
@@ -60,12 +62,14 @@ end
 foo [1, 2, 3]
 # => [[1, 2, 3]]
 
+############################################################
 
-# https://docs.ruby-lang.org/ja/latest/method/Module/i/class_eval.html
-# Module#class_evalは、引数に文字列またはブロックを受け取る
+# Module#class_eval
+## Module#class_evalは、引数に文字列またはブロックを受け取る
+## https://docs.ruby-lang.org/ja/latest/method/Module/i/class_eval.html
 
-# 引数が文字列の場合は、定数とクラス変数のスコープは、自身のモジュール定義式
-# 内と同じスコープになる
+## 引数が文字列の場合は、定数とクラス変数のスコープは、自身のモジュール定義式
+## 内と同じスコープになる
 
 class C1
   X = 'X'
@@ -83,8 +87,7 @@ C1.new.foo
 # => "X"
 "c_var"
 
-# 引数がブロックの場合は、定数とクラス変数のスコープはブロックの外側のスコープになる
-
+## 引数がブロックの場合は、定数とクラス変数のスコープはブロックの外側のスコープになる
 class C2
   X = 'X'
   @@c_var = 'c_var'
@@ -100,8 +103,9 @@ end
 C2.new.foo
 # => "global X"
 
-# 例外処理
+############################################################
 
+# 例外処理
 begin
   # 何らかの処理
 begin
@@ -138,6 +142,7 @@ end
 # => #<ZeroDivisionError: divided by 0>
 # => "ensure"
   
+############################################################  
   
 # class, moduleの探索順序
   
@@ -169,3 +174,32 @@ end
 
 p C.ancestors
 # => [M1, M2, C, Object, Kernel, BasicObject]
+
+############################################################
+  
+# 正規表現
+# \wは大文字、小文字、数字とアンダーバー(_)にマッチします。
+
+############################################################
+  
+# モジュールのネスト  
+## Module.nestingはネストの状態を表示する。
+module S
+  p Module.nesting # => [S]
+end
+
+## インデントでネストの状態を記述した場合は、ネストの状態すべてが表示される
+module S
+  module C
+    p Module.nesting # => [S::C, S]
+  end
+end
+
+## プレフィックスでネストを記述した場合は、内側にあるモジュールしかネストの状態は表示されない
+module S::C
+  p Module.nesting # => [S::C]
+end
+  
+
+  
+  
